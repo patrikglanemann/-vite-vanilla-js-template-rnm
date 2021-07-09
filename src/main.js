58d2c85c8fb69442121e2123529566cd39f7ead0
PatrikG <Patrik0Gn@gmail.com>
2021-07-09 18:11:33 +0200

    Refactor functions

--- src/main.js
+++ src/main.js
@@ -1,12 +1,64 @@
 const url = "https://rickandmortyapi.com/api/character/";
 
 const content = document.querySelector(".content");
+window.addEventListener("scroll", () => {
+  if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+    console.log("miau");
+
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        //throw new Error("An error occured");
+        const nextPage = data.info.next; //info is name of array from api results
+        getChars(nextPage);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+});
 
 const fetchDataBtn = document.createElement("button");
 fetchDataBtn.classList.add("content__fetch-btn");
 fetchDataBtn.textContent = "Fetch Data";
 content.append(fetchDataBtn);
 fetchDataBtn.addEventListener("click", () => {
+  let newUrl = "https://rickandmortyapi.com/api/character/";
+  fetchCharList(newUrl);
+});
+
+function fetchCharList(newUrl) {
+  fetch(newUrl)
+    .then((resp) => resp.json())
+    .then((data) => {
+      //throw new Error("An error occured");
+      const characterList = data.results; //results is name of array from api results
+      createCharCards(characterList);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function createCharCards(characterList) {
+  console.log(characterList);
+  characterList.map((character) => {
+    const section = document.createElement("section");
+    section.classList.add("character");
+
+    const h2 = document.createElement("h2");
+    h2.textContent = character.name;
+
+    const img = document.createElement("img");
+    img.src = character.image;
+    img.alt = `Picture of ${character.name}`;
+    section.append(h2);
+    section.append(img);
+    content.append(section);
+  });
+}
+
+function getChars(url) {
   fetch(url)
     .then((resp) => resp.json())
     .then((data) => {
@@ -30,4 +82,4 @@ fetchDataBtn.addEventListener("click", () => {
     .catch((error) => {
       console.log(error);
     });
-});
+}
